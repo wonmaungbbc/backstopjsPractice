6aad523ce1888b7e2264a457b37b8e2e1a937a1b
Won Maung Thein <wonmaungthein@gmail.com>
2018-11-28 12:33:33 +0000

    Added backstopjs file

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,54 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_My_BackstopJS_testing_practice_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20181128-122309/backstop_default_My_BackstopJS_testing_practice_Homepage_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_My_BackstopJS_testing_practice_Homepage_0_document_0_phone.png",
+        "label": "My BackstopJS testing practice Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "file:///Users/wonmaungthein/Desktop/backstopjsPractice/index.html",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_My_BackstopJS_testing_practice_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20181128-122309/backstop_default_My_BackstopJS_testing_practice_Homepage_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_My_BackstopJS_testing_practice_Homepage_0_document_1_tablet.png",
+        "label": "My BackstopJS testing practice Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "file:///Users/wonmaungthein/Desktop/backstopjsPractice/index.html",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
